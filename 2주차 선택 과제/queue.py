03a246d7bd02c52616ce45412a011a2007130ba7
KO BYUNGHWA <42263086+bbbyung2@users.noreply.github.com>
2020-01-14 03:05:37 +0900

    Add files via upload

---
+++ 2주차 선택 과제/queue.py
@@ -0,0 +1,26 @@
+class queue:
+  def __init__(self):
+    self.arr = []
+    self.front = 0
+    self.rear = 0
+
+  def push(self, data):
+    self.arr.append(data)
+    self.rear += 1
+
+  def isEmpty(self):
+    if self.rear == self.front:
+      return True
+    return False
+
+  def pop(self):
+    if self.isEmpty():
+      return False
+    output = self.arr[self.front]
+    self.front += 1
+    return output
+
+  def peek(self):
+    if self.isEmpty():
+      return False
+    return self.arr[self.front]
